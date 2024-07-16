7fb9643bed1f4b3e4adbd1d801fe17788f5aa025
arelice <141432992+arelice@users.noreply.github.com>
2024-07-16 21:30:08 +0800

    Update main.go

--- router/main.go
+++ router/main.go
@@ -10,17 +10,32 @@ import (
 	"strings"
 )
 
-func SetRouter(router *gin.Engine, buildFS embed.FS, indexPageUser []byte, indexPageAdmin []byte) {
+//go:embed web web-admin
+var BuildFS embed.FS
+
+func SetRouter(router *gin.Engine) {
 	SetApiRouter(router)
 	SetDashboardRouter(router)
 	SetRelayRouter(router)
+
 	frontendBaseUrl := os.Getenv("FRONTEND_BASE_URL")
 	if common.IsMasterNode && frontendBaseUrl != "" {
 		frontendBaseUrl = ""
 		common.SysLog("FRONTEND_BASE_URL is ignored on master node")
 	}
+
 	if frontendBaseUrl == "" {
-		SetWebRouter(router, buildFS, indexPageUser, indexPageAdmin)
+		// 读取嵌入的index.html文件
+		indexPageUser, err := BuildFS.ReadFile("web/index.html")
+		if err != nil {
+			common.SysError("Failed to read user index.html: " + err.Error())
+		}
+		indexPageAdmin, err := BuildFS.ReadFile("web-admin/index.html")
+		if err != nil {
+			common.SysError("Failed to read admin index.html: " + err.Error())
+		}
+
+		SetWebRouter(router, indexPageUser, indexPageAdmin)
 	} else {
 		frontendBaseUrl = strings.TrimSuffix(frontendBaseUrl, "/")
 		router.NoRoute(func(c *gin.Context) {
